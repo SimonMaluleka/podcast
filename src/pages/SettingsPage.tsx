f3a02ed94a5235651e4bf114f591d1eb98ad0df9
Simon Maluleka <Sicamaluks2014@gmail.com>
2024-03-21 18:31:34 +0200

    created settings page

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,8 +1,15 @@
-import { Container } from '@mui/material'
+import { Box, Container } from '@mui/material'
 
 const SettingsPage = () => {
   return (
-    <Container sx={{mt:'180px'}}>SettingsPage</Container>
+    <Container sx={{mt:'180px', display:'flex'}}>
+        <Box sx={{width:"30%"}}>
+            settings leftside
+        </Box>
+        <Box sx={{width:"70%"}}>
+            settings main
+        </Box>
+    </Container>
   )
 }
 
